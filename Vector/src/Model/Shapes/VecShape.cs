f3b9b68ab0b2b53b68d83111bd8b0ff180757c19
Danila <kdv0807@gmail.com>
2020-06-24 18:22:31 +0700

    add VecShape, line and ellipse

---
+++ Vector/src/Model/Shapes/VecShape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector.src.Model.Shapes
+{
+    class VecShape
+    {
+        private double outlineSize;
+        private Color outlineColor;
+        private Color shapeColor;
+        private double tiltAngle;
+
+        public Color ShapeColor { get => shapeColor; set => shapeColor = value; }
+        public Color OutlineColor { get => outlineColor; set => outlineColor = value; }
+        public double OutlineSize { get => outlineSize; set => outlineSize = value; }
+        public double TiltAngle { get => tiltAngle; set => tiltAngle = value; }
+    }
+}
